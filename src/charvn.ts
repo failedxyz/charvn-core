1bd2e39868b9685b4e4ae6a5816713a5af6ba505
Scott Wu <scott.wu.hjx@gmail.com>
2017-01-20 18:15:44 -0500

    Export language in module root

--- src/charvn.ts
+++ src/charvn.ts
@@ -5,3 +5,6 @@ export { GameState } from "./gameState";
 export { Scene } from "./scene";
 export { Sprite } from "./sprite";
 export { VNComponent } from "./vnComponent";
+
+import * as Language from "./language";
+export { Language };
